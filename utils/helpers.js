fa5daddb35a88d195aba8de16a58dc711eb98fc8
Alex LaPierre <aslapierre7@gmail.com>
2024-06-18 12:16:55 -0500

    reverted changes

---
+++ utils/helpers.js
@@ -0,0 +1,13 @@
+// const Handlebars = require('handlebars');
+
+// const isLoggedIn = function(user, options) {
+//     return user ? options.fn(this) : options.inverse(this);
+// };
+
+// Handlebars.registerHelper('ifLoggedIn', isLoggedIn);
+
+// const helpers = {
+//     ifLoggedIn: isLoggedIn,
+// };
+
+// module.exports = helpers;
